7645e34a61b180073d7233c2898b47b375dac478
Christian <csoronellas@emagister.com>
2012-02-10 21:03:59 +0100

    Added unit test for SensioConnectService.

--- src/SFBCN/WebsiteBundle/Tests/Service/SensioConnectServiceTest.php
+++ src/SFBCN/WebsiteBundle/Tests/Service/SensioConnectServiceTest.php
@@ -10,10 +10,17 @@ namespace SFBCN\WebsiteBundle\Tests\Service;
 
 use SFBCN\WebsiteBundle\Service\SensioConnectService;
 
+/**
+ * Test the integration between SensioConnect and Symfony Barcelona
+ *
+ * @category UnitTests
+ * @package SFBCN\WebsiteBundle\Tests
+ * @subpackage Service
+ */
 class SensioConnectServiceTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var \SFBCN\WebsiteBundle\Service\SensionConnectService
+     * @var \SFBCN\WebsiteBundle\Service\SensioConnectService
      */
     private $sensioConnectService;
 
@@ -29,5 +36,13 @@ class SensioConnectServiceTest extends \PHPUnit_Framework_TestCase
         $this->sensioConnectService = null;
     }
 
+    public function testGetGroupInfo()
+    {
+        $result = $this->sensioConnectService->setGroupName('symfony-barcelona')
+                                             ->getGroupInfo();
 
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('club_url', $result);
+        $this->assertEquals('https://connect.sensiolabs.com/club/symfony-barcelona', $result['club_url']);
+    }
 }
