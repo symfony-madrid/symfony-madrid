8254c1f716fbf3860f7bc5e48560dbd618a58648
Ricard Clau <ricard.clau@gmail.com>
2012-02-14 14:11:58 +0100

    fix to only store in apc if apc actually is loaded

--- src/SFBCN/WebsiteBundle/Service/RssReaderService.php
+++ src/SFBCN/WebsiteBundle/Service/RssReaderService.php
@@ -41,19 +41,39 @@ class RssReaderService
     public function parseRss()
     {
         $apcKey = 'sfbcnrss_' . md5($this->urlResource);
-        if (extension_loaded('apc') && apc_exists($apcKey)) {
-            $rss = simplexml_load_string(apc_fetch($apcKey));
-        } else {
-            /**
-             * Symfony.es did not work with simplexml_load_file in PHP5.3.6
-             */
-            $rss = simplexml_load_string(file_get_contents($this->urlResource));
-            if (!$rss) {
-                return array();
+        if (extension_loaded('apc')) {
+            if (apc_exists($apcKey)) {
+                $rss = simplexml_load_string(apc_fetch($apcKey));
+            } else {
+                $rss = $this->getRSSInfo($this->urlResource);
+
+                if (!$rss) {
+                    apc_store($apcKey, array(), 3600);
+                } else {
+                    apc_store($apcKey, $rss->asXML(), 3600);
+                }
             }
-            apc_store($apcKey, $rss->asXML(), 3600);
+        } else {
+            $rss = $this->getRSSInfo($this->urlResource);
         }
 
-        return $rss->channel[0]->item;
+        if (!$rss) {
+            return array();
+        } else {
+            return $rss->channel[0]->item;
+        }
+    }
+
+    /**
+     * Connects to RSS Url resource and obtains info
+     * @param string $url
+     * @return \SimpleXMLElement
+     */
+    private function getRSSInfo($url)
+    {
+        /**
+         * Symfony.es did not work with simplexml_load_file in PHP5.3.6
+         */
+        return simplexml_load_string(file_get_contents($this->urlResource));
     }
 }
