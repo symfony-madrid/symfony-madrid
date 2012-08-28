ccd3293837fd5ce43238424a30cb2bc8b8915793
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2012-08-28 17:09:46 +0200

    .

--- web/app.php
+++ web/app.php
@@ -3,23 +3,12 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-umask(0000);
-
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
-
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppKernel.php';;
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
